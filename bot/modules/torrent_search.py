b11992f7bdb4392e62706fdd36a0f7325f1234f5
Tj-Bots <tjbots123@gmail.com>
2026-06-26 01:24:10 +0300

    Update torrent_search.py

--- bot/modules/torrent_search.py
+++ bot/modules/torrent_search.py
@@ -250,7 +250,7 @@ async def torrentSearchUpdate(_, query):
     key = key[1].strip() if len(key) > 1 else None
     data = query.data.split()
     if user_id != int(data[1]):
-        await query.answer("Not Yours!", show_alert=True)
+        await query.answer("𝙏𝙝𝙖𝙩'𝙨 𝙉𝙤𝙩 𝙔𝙤𝙪𝙧𝙨...🤧", show_alert=True)
     elif data[2].startswith('api'):
         await query.answer()
         button = __api_buttons(user_id, data[2])
